8dff2ab6e105dd31f02a6cff56c6158a6faa8ed1
seulks <108436467+seulks@users.noreply.github.com>
2024-01-14 17:44:26 -0800

    Initial Commit

---
+++ CPSC5330_Assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CPSC5330_Assignment1
+//
+//  Created by Ellie Kim on 1/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
